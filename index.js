224e78c8842453c927f3f4326a42a244f76c1689
Ryan Nutt <ryan@nutt.com>
2024-10-07 12:45:31 -0500

    take out wrapper function;

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
-(function () {
-    'use strict';
+// (function () {
+//     'use strict';
 
-    // Get course ID
-    let courseID = ENV.current_context.id;
-    let host = location.protocol + '//' + location.host;
-    console.info('courseID', courseID);
-    console.info('host', host);
+// Get course ID
+let courseID = ENV.current_context.id;
+let host = location.protocol + '//' + location.host;
+console.info('courseID', courseID);
+console.info('host', host);
 
-    let interval = setInterval(function () {
-        let lateEls = document.querySelectorAll('.gradebook-cell.late');
-        console.info(lateEls);
-        lateEls.forEach(function (el) {
-            let endEl = el.querySelector('.Grid__GradeCell__EndContainer');
-            if (!endEl.innerHTML) {
-                let parentCell = endEl.closest('.slick-cell');
-                let assignmentID = '';
-                if (parentCell) {
-                    parentCell.classList.forEach(function (className) {
-                        if (className.includes('assignment_')) {
-                            assignmentID = className.replace('assignment_', '');
-                        }
-                    });
-                }
-
-                let studentID = '';
-                let studentEl = endEl.closest('.slick-row');
-                if (studentEl) {
-                    studentEl.classList.forEach(function (className) {
-                        if (className.includes('student_')) {
-                            studentID = className.replace('student_', '');
-                        }
-                    });
-                }
-
-                console.log('courseID', courseID);
-                console.log('studentID', studentID);
-                console.log('assignmentID', assignmentID);
-
-
-                if (studentID && assignmentID) {
-                    let url = `${host}/api/v1/courses/${courseID}/assignments/${assignmentID}/submissions/${studentID}`;
-                    fetch(url)
-                        .then(response => response.json())
-                        .then(data => {
-                            if (data.late) {
-                                let daysLate = Math.round(data.seconds_late / 60 / 60 / 24 * 100 / 100);
-                                endEl.innerHTML = '<span style="font-size:75%;">(' + daysLate + ')</span>';
-                            }
-                        });
-                }
+let interval = setInterval(function () {
+    let lateEls = document.querySelectorAll('.gradebook-cell.late');
+    console.info(lateEls);
+    lateEls.forEach(function (el) {
+        let endEl = el.querySelector('.Grid__GradeCell__EndContainer');
+        if (!endEl.innerHTML) {
+            let parentCell = endEl.closest('.slick-cell');
+            let assignmentID = '';
+            if (parentCell) {
+                parentCell.classList.forEach(function (className) {
+                    if (className.includes('assignment_')) {
+                        assignmentID = className.replace('assignment_', '');
+                    }
+                });
             }
-        });
-    }, 2_500);
-})
+
+            let studentID = '';
+            let studentEl = endEl.closest('.slick-row');
+            if (studentEl) {
+                studentEl.classList.forEach(function (className) {
+                    if (className.includes('student_')) {
+                        studentID = className.replace('student_', '');
+                    }
+                });
+            }
+
+            console.log('courseID', courseID);
+            console.log('studentID', studentID);
+            console.log('assignmentID', assignmentID);
+
+
+            if (studentID && assignmentID) {
+                let url = `${host}/api/v1/courses/${courseID}/assignments/${assignmentID}/submissions/${studentID}`;
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.late) {
+                            let daysLate = Math.round(data.seconds_late / 60 / 60 / 24 * 100 / 100);
+                            endEl.innerHTML = '<span style="font-size:75%;">(' + daysLate + ')</span>';
+                        }
+                    });
+            }
+        }
+    });
+}, 2_500);
+// })
